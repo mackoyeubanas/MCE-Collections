b3cab0882bc8caeca88eaa785f344309d54e5f36
mackoyeubanas <90250663+mackoyeubanas@users.noreply.github.com>
2022-02-24 20:44:24 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     env: {
         "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "mongodb+srv://admin:admin131@zuitt.cfuxf.mongodb.net/MCE_2?retryWrites=true&w=majority",
+        "MONGODB_URL": "mongodb",
         "ACCESS_TOKEN_SECRET": "mackoy12345678",
         "REFRESH_TOKEN_SECRET": "mackoy12345678910",
-        "PAYPAL_CLIENT_ID": "AYGxZakKP9wrysmrIddW1_ayUSjFq3DwSL5Z20ooCOzFL7acNXJX624PTym1HFUMcOPR1jnyupdqWMrB",
+        "PAYPAL_CLIENT_ID": "paypal",
         "CLOUD_UPDATE_PRESET": "my-upload",
-        "CLOUD_NAME": "mce-collections",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/mce-collections/image/upload"
+        "CLOUD_NAME": "",
+        "CLOUD_API": "your image folder"
     }
 }
